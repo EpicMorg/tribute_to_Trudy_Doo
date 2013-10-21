b88de6bf6ccf7eac4874f95be27897cdf6e33971
STAM <stam@epicm.org>
2013-10-21 23:58:41 +0400

    minor_update1

--- src/Installer/Installer_GUI/frm_main.Designer.cs
+++ src/Installer/Installer_GUI/frm_main.Designer.cs
@@ -66,6 +66,12 @@
             this.tabPage7 = new System.Windows.Forms.TabPage();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.tabs_main.SuspendLayout();
             this.tab_main.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -166,6 +172,8 @@
             // panel3
             // 
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label4);
             this.panel3.Controls.Add(this.pictureBox2);
             this.panel3.Location = new System.Drawing.Point(-7, -1);
             this.panel3.Name = "panel3";
@@ -316,6 +324,8 @@
             // 
             this.panel4.BackColor = System.Drawing.Color.White;
             this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.label6);
             this.panel4.Controls.Add(this.pictureBox3);
             this.panel4.Location = new System.Drawing.Point(-7, -1);
             this.panel4.Name = "panel4";
@@ -377,6 +387,8 @@
             // 
             this.panel7.BackColor = System.Drawing.Color.White;
             this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel7.Controls.Add(this.label7);
+            this.panel7.Controls.Add(this.label8);
             this.panel7.Controls.Add(this.pictureBox4);
             this.panel7.Location = new System.Drawing.Point(-7, -1);
             this.panel7.Name = "panel7";
@@ -466,6 +478,63 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Много текста не осилил";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Много текста не осилил";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(9, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(256, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Welcome to Installiation Wizzard";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Много текста не осилил";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(9, 3);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(256, 18);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Welcome to Installiation Wizzard";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 21);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Много текста не осилил";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(9, 3);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(256, 18);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Welcome to Installiation Wizzard";
+            // 
             // frm_main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -544,6 +613,12 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn txt_name;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
 
 
     }
